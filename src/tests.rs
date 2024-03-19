e41046101d5610a5e50eee13e05f58730d0aba9e
Algorhythm-sxv <sebastian.venter@gmail.com>
2024-03-19 21:44:25 +0100

    add another test position

--- src/tests.rs
+++ src/tests.rs
@@ -11,24 +11,30 @@ fn test_probe_kpvk() {
         if !init {
             panic!("failed to init TBs")
         }
-        let test_pos_win = "6k1/8/8/3P4/4K3/8/8/8 w - - 0 1";
+        let test_pos_wins = [
+            "6k1/8/8/3P4/4K3/8/8/8 w - - 0 1",
+            "8/7k/1p6/1P6/7K/8/8/8 w - - 0 1",
+        ];
         let test_pos_draw = "6k1/8/8/3P4/4K3/8/8/8 b - - 0 1";
 
-        let test_board_win = Board::from_str(test_pos_win).unwrap();
-        let test_board_draw = Board::from_str(test_pos_draw).unwrap();
+        for win_pos in test_pos_wins {
+            let test_board_win = Board::from_str(win_pos).unwrap();
 
-        let wdl_win = tb_probe_wdl(
-            test_board_win.colors(Color::White).0,
-            test_board_win.colors(Color::Black).0,
-            test_board_win.pieces(Piece::King).0,
-            test_board_win.pieces(Piece::Queen).0,
-            test_board_win.pieces(Piece::Rook).0,
-            test_board_win.pieces(Piece::Bishop).0,
-            test_board_win.pieces(Piece::Knight).0,
-            test_board_win.pieces(Piece::Pawn).0,
-            0, // no ep square
-            test_board_win.side_to_move() == Color::White,
-        );
+            let wdl_win = tb_probe_wdl(
+                test_board_win.colors(Color::White).0,
+                test_board_win.colors(Color::Black).0,
+                test_board_win.pieces(Piece::King).0,
+                test_board_win.pieces(Piece::Queen).0,
+                test_board_win.pieces(Piece::Rook).0,
+                test_board_win.pieces(Piece::Bishop).0,
+                test_board_win.pieces(Piece::Knight).0,
+                test_board_win.pieces(Piece::Pawn).0,
+                0, // no ep square
+                test_board_win.side_to_move() == Color::White,
+            );
+            assert!(wdl_win == 4);
+        }
+        let test_board_draw = Board::from_str(test_pos_draw).unwrap();
         let wdl_draw = tb_probe_wdl(
             test_board_draw.colors(Color::White).0,
             test_board_draw.colors(Color::Black).0,
@@ -42,7 +48,6 @@ fn test_probe_kpvk() {
             test_board_draw.side_to_move() == Color::White,
         );
 
-        assert!(wdl_win == 4);
         assert!(wdl_draw == 2);
         tb_free();
     }
